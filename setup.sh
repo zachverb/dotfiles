6bcadddea7c6518039108d90db4a073d75b8924b
zachverb <zverbeck@uber.com>
2016-02-18 14:56:34 -0800

    Renaming

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 BACKUP_OLD_DOT_FILES_FOLDER=~/olddotfiles
 DOT_FILES_FOLDER="~/dotfiles"
-DOT_FILES_AND_FOLDERS=".vimrc .tmux.conf .zshrc .uprofile"
+DOT_FILES_AND_FOLDERS="vimrc tmux.conf zshrc uprofile oh-my-zsh"
 
 echo "Backing up old dot files in $BACKUP_OLD_DOT_FILES_FOLDER"
 mkdir "$BACKUP_OLD_DOT_FILES_FOLDER"
@@ -16,7 +16,7 @@ done
 echo "Creating sym links to new dot files"
 for dot_file in $DOT_FILES_AND_FOLDERS
 do
-  sym_link_cmd="ln -vs $DOT_FILES_FOLDER/$dot_file ~/$dot_file"
+  sym_link_cmd="ln -vs $DOT_FILES_FOLDER/$dot_file ~/.$dot_file"
   eval $sym_link_cmd
 done
 
